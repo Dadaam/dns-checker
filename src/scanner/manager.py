098483eb99dd9e3199dd8acf13102f6dec8a538c
Adam Chtourou <adam.chtourou@oteria.fr>
2026-01-15 10:57:53 +0100

    feat: implement robots.txt fetcher and update manager

--- src/scanner/manager.py
+++ src/scanner/manager.py
@@ -1,5 +1,6 @@
 from .dns_lookup import DNSLookup
 from .whois_lookup import WhoisLookup
+from .robots_lookup import RobotsLookup
 from typing import Dict, Any
 
 class ScannerManager:
@@ -11,13 +12,15 @@ class ScannerManager:
         self.domain = domain
         self.dns_scanner = DNSLookup(domain)
         self.whois_scanner = WhoisLookup()
+        self.robots_scanner = RobotsLookup()
 
     def scan_all(self) -> Dict[str, Any]:
         """
-        Perform a full scan (DNS + WHOIS).
+        Perform a full scan (DNS + WHOIS + Robots.txt).
         """
         return {
             "domain": self.domain,
             "dns": self.dns_scanner.get_all_records(),
-            "whois": self.whois_scanner.get_whois_info(self.domain)
+            "whois": self.whois_scanner.get_whois_info(self.domain),
+            "robots": self.robots_scanner.get_robots_txt(self.domain)
         }
