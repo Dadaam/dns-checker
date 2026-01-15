054a4f2513eeb08ff65236e6ca9336422d835245
Adam Chtourou <adam.chtourou@oteria.fr>
2026-01-15 11:41:54 +0100

    feat(core): implement Node and Edge data models

---
+++ src/models/graph.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import List, Set, Optional
+
+class NodeType(Enum):
+    DOMAIN = "DOMAIN"
+    IP_V4 = "IP_V4"
+    IP_V6 = "IP_V6"
+    TLD = "TLD"
+    SERVICE = "SERVICE" # For SRV records
+
+class EdgeType(Enum):
+    A = "A"
+    AAAA = "AAAA"
+    CNAME = "CNAME"
+    NS = "NS"
+    MX = "MX"
+    PTR = "PTR"
+    TXT = "TXT"
+    SRV = "SRV"
+    PARENT = "PARENT" # Deduced parent
+    NEIGHBOR = "NEIGHBOR" # IP Neighbor
+
+@dataclass(frozen=True)
+class Node:
+    value: str
+    type: NodeType
+    
+    def __repr__(self):
+        return f"{self.type.value}:{self.value}"
+
+@dataclass(frozen=True)
+class Edge:
+    source: Node
+    target: Node
+    type: EdgeType
+    
+    def __repr__(self):
+        return f"{self.source} --[{self.type.value}]--> {self.target}"
